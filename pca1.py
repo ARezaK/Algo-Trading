c3fec855371f908933c40196cfbf67163b4e381f
ARezaK <ameerkambod@Ameers-MacBook-Air.local>
2015-02-27 14:45:06 -0600

    updates

--- pca1.py
+++ pca1.py
@@ -2,7 +2,20 @@ import pandas as pd
 import numpy as np
 import pandas.rpy.common as com
 import rpy2.robjects as robjects
+import matplotlib.pyplot as plt
 import time
+
+#init instance to r
+r = robjects.r
+rprint = robjects.globalenv.get("print")
+plot = robjects.r.plot
+
+robjects.r("""
+    library(ggbiplot)
+    library(gplots)
+    library(reshape)
+    library(ggplot2)
+    """)
 
 def init_data(ajdusted_or_unadjust):
     adj = 0
@@ -60,10 +73,68 @@ def init_data(ajdusted_or_unadjust):
     newdataset['CL2_0'] = table2
     newdataset['TY_0'] = table3
 
-    #for some reason when reading in from excel we get some NaN for CL2_0 values that dont exist so delete those rows
+    #for some reason when reading in from excel we get some NaN for CL2_0 valuebs that dont exist so delete those rows
     dframe = newdataset[pd.notnull(newdataset['CL2_0'])]
-    print dframe
     return dframe
+
+def plot_dataframe_in_matplotlib(the_dataframe):
+    #calculate percent variance
+
+    the_dataframe.pct_change(90).plot(subplots=True)
+
+    #for plotting dataframe in matlplotlib
+    the_dataframe.plot()
+    plt.show()
+
+
+
+
+
+
+def plot_dataframe_in_r():
+    """THIS SHIT DOES NOT WORK"""
+
+    import math, datetime
+    import rpy2.robjects.lib.ggplot2 as ggplot2
+    import rpy2.robjects as ro
+    from rpy2.robjects.packages import importr
+    #for plotting dataframe in r
+    dframe = init_data("adjusted")
+
+    #add back data column
+    #init dataframe
+    dataset = pd.DataFrame()
+    #read in first table and add it to dataframe
+    frame = pd.read_csv("/Users/ameerkambod/Trading/Algo-Trading/P11_PCA_Data1/Sheet1-Table 1.csv")
+    dataset = dataset.append(frame,ignore_index=True)
+
+    del dataset['Symbol']
+    del dataset['Delivery']
+    del dataset['UClose1']
+    del dataset['Close']
+
+    dframe['Date'] = dataset
+    print dframe
+
+    #create an r dataframe
+    r_dataframe = com.convert_to_r_dataframe(dframe)
+    robjects.globalenv['dataframe'] = r_dataframe
+
+
+
+    robjects.r("""
+    pdf("ggplot.pdf")
+    #par(mfrow=c(3,1), lheight=1)
+
+
+    Molten <- melt(dataframe, id.vars="Date")
+    textplot(capture.output(ggplot(Molten, aes(x = Date, y=value)) + geom_point()))
+    print("DONE")
+    dev.off()
+
+
+    """)
+
 
 def do_pca_on_data(ajdusted_or_unadjust):
     if 'adjusted' in ajdusted_or_unadjust:
@@ -71,10 +142,6 @@ def do_pca_on_data(ajdusted_or_unadjust):
     else:
         dframe = init_data("unadjust")
 
-    #init instance to r
-    r = robjects.r
-    rprint = robjects.globalenv.get("print")
-    plot = robjects.r.plot
 
 
     #create an r dataframe
@@ -83,16 +150,13 @@ def do_pca_on_data(ajdusted_or_unadjust):
 
 
     print "----"
-    print robjects.r.ls()
+    #print robjects.r.ls()
 
-    robjects.r("""
-    library(ggbiplot)
-    library(gplots)
-    """)
+
 
     #change output name
     robjects.r("""
-    pdf("scale_center_unadjustedclose.pdf")
+    pdf("scale_center_adjustedclose.pdf")
     """)
 
     robjects.r("""
@@ -109,11 +173,15 @@ def do_pca_on_data(ajdusted_or_unadjust):
     robjects.r("""
     #log.dataframe = log(dataframe)
     dataframe.pca <- prcomp(dataframe, center=TRUE, scale. = TRUE)
+    print("loadings")
+    print(dataframe.pca$x)
     """)
+
+
 
     robjects.r("""
     print("PCA Output")
-    print(dataframe.pca)
+    #print(dataframe.pca)
     textplot(capture.output(dataframe.pca))
     textplot(capture.output(summary(dataframe.pca)))
 
@@ -130,7 +198,20 @@ def do_pca_on_data(ajdusted_or_unadjust):
 
 
 
-do_pca_on_data('unadjust')
+#plot_dataframe_in_r()
+#do_pca_on_data('adjusted')
+
+#to grab an object from r
+#r_dataframe = com.convert_robj(robjects.globalenv['dataframe'])
+
+dframe = init_data('adjusted')
+plot_dataframe_in_matplotlib(dframe)
+
+
+
+
+
+
 
 """
 foo = robjects.r['g']
